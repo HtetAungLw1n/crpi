9e2ad6de891b017a01d78cb1f371dcf3aa9bc13d
HtetAungLwin <htetaunglwin.canon@gmail.com>
2025-06-11 14:55:36 +0630

    research page done

--- research.php
+++ research.php
@@ -109,9 +109,9 @@
     </nav>
 
     <div class="bg-deep-blue font-playfair pt-40 pb-20 text-white text-center">
-        <div class="text-4xl font-bold mb-2">Programs</div>
-        <div class="text-xl font-notoserif">Home > Programs > Research Fellow – Center for Research, Policy and
-            Innovation (CRPI)
+        <div class="text-4xl font-bold mb-2">Policy Action Research</div>
+        <div class="text-xl font-notoserif">Home > Programs > CRPI Policy Action Research Summer Program with the Center
+            for Research, Policy and Innovation (CRPI)
         </div>
     </div>
 </section>
@@ -123,9 +123,8 @@
 
     <!-- first text-area  -->
     <div class="font-notoserif">
-        <div class="text-xl lg:text-4xl font-bold font-playfair my-8">Research Fellow - Center for Research, Policy and
-            Innovation
-            (CRPI)</div>
+        <div class="text-xl lg:text-4xl font-bold font-playfair my-8">CRPI Policy Action Research Summer Program with
+            the Center for Research, Policy and Innovation (CRPI)</div>
 
         <!-- date and time -->
         <div class="flex items-center justify-start text-deep-blue">
@@ -139,115 +138,45 @@
             <div class="flex items-start lg:items-center gap-2 border-l-2 border-gray-200 px-2"><img
                     src="./icons/category-name.svg" alt="category">Category Name</div>
         </div>
-
-        <!-- status  -->
-        <div class="bg-[#D5D5D5] p-8 rounded-lg w-full mt-8 space-y-2">
-            <div class="flex lg:items-center items-start">
-                <div class="w-1/2 lg:w-52">Hours</div>
-                <div class="w-1/2">: Full-time or part-time</div>
-            </div>
-            <div class="flex lg:items-center items-start">
-                <div class="w-1/2 lg:w-52">Duration</div>
-                <div class="w-1/2">: Summer 2023 (With the possibility of continuation to Fall 2023 and beyond)</div>
-            </div>
-            <div class="flex lg:items-center items-start">
-                <div class="w-1/2 lg:w-52">Pay</div>
-                <div class="w-1/2">: Stipend; negotiable</div>
-            </div>
-            <div class="flex lg:items-center items-start">
-                <div class="w-1/2 lg:w-52">Project Primary Site</div>
-                <div class="w-1/2">: Virtual</div>
-            </div>
-
-        </div>
-
     </div>
 
     <!-- second text-area  -->
-    <div class="font-notoserif">
+    <div class="font-notoserif pb-20 border-b-2 border-gray-300">
         <!-- paragraph -->
         <div class="mt-8">
             <div class="indent-4">
-                The Burmese American Community Institute (BACI) is a dynamic, forward-looking, and growing organization.
-                We
-                seek motivated, committed, and enthusiastic candidates to join our team as a Lead Researcher at the
-                Center for Research, Policy and Innovation (CRPI).
+                In this program, researchers and scholars participate in intensive scientific group research projects
+                from during the summer (June – August), primarily utilizing participatory action research (PAR)
+                methodology. In consultation with the CRPI Directors and under the leadership of research fellows or
+                lead researchers, teams of 5 to 7 researchers and scholars will form and propose topics for research.
+                Each research team is expected to produce an academic paper that includes its main findings and
+                solution-oriented policy recommendations and present it to relevant stakeholders at the end of the
+                project. Project deliverables include:
             </div>
 
-            <div class="mt-4 block"> In collaborative partnership with the National
-                University of the Union of Myanmar – Global Campus and other Universities in the USA, CRPI facilitates
-                and enables collaborative research and action projects with various researchers in Indiana, as well as
-                with Burmese scholars, other international scholars, and scholars across the US whose research and
-                projects might be applicable to the Burmese American community, to Myanmar, and to Southeast Asia.</div>
-
-            <div class="mt-4 block"> Particularly, a 10-week intensive Participatory Action Research (PAR) scientific
-                research project with
-                one’s chosen topic this summer is expected to produce academically written paper with its main findings
-                and solution-oriented policy recommendations to be presented to the stakeholders at the end of the
-                project.</div>
-        </div>
-
-        <!-- qualifications -->
-        <div>
-            <div class="text-[#5273FA] my-6 mt-10 text-2xl font-semibold font-playfair">Qualifications</div>
-            <ul class="list-outside list-disc pl-6">
-                <li>Current faculty member at a University, Graduate student, doctoral degree, advanced or terminal
-                    degree preferred (College Senior may be considered).</li>
-                <li>Strong familiarity with research methodology and/or experience</li>
-                <li>Commitment to participatory action research as a method of engaging and empowering youth to create
-                    positive social change</li>
-                <li>Outstanding written and verbal communication skills; strong writing highly desired</li>
-                <li>Able to work in a fast-paced environment/meet deadlines</li>
-                <li>Proficiency in Microsoft Office and internet applications</li>
-                <li>Demonstrate high level of professionalism, respect, and personal integrity</li>
-                <li>Be a team player; able to collaborate with others</li>
-                <li>International experience – high degree of cultural sensitivity and competence</li>
-                <li>Enthusiasm for <span class="text-[#5273FA]"><a href="#">CRPI mission</a></span></li>
-                <li>Flexibility and availability/willingness to work some weekends</li>
-            </ul>
-        </div>
-
-        <!-- responsibilites -->
-        <div>
-            <div class="text-[#5273FA] my-6 mt-10 text-2xl font-semibold font-playfair">Responsibilites</div>
-            <ul class="list-outside list-disc ml-4">
-                <li>Complete orientation and training (will take place during the first week of the program)</li>
-                <li>Successfully implement the program curriculum, leading the research process from beginning to end
+            <ul class="list-outside list-disc pl-6 mt-8">
+                <li>A 20-30 page academic paper with policy and action recommendations</li>
+                <li>An executive summary of the paper that outlines the main findings of the study and policy
+                    recommendations</li>
+                <li>PowerPoint slides for the final research/project presentation</li>
+                <li>Literature reviewed, documentation relating to the methodology (such as survey forms), and raw data.
                 </li>
-                <li>Introduce and explain key concepts in research methodology, making sure students or project team
-                    members fully grasp each step and why it matters</li>
-                <li>Lead 4-7 or more students/scholars as the groups engages in research projects, helping each student
-                    and team overcome challenges as they arise, and ensuring the active participation of all group
-                    members in the research and the implementation process of the project</li>
-                <li>Attend staff or larger groups meeting, prepare meeting notes</li>
-                <li>Closely work with Program Assistants and Interns</li>
-                <li>Fully document daily reflections, observations, and program activities</li>
-                <li>Prepare bi-weekly reports to Project Director or Supervisor</li>
-                <li>Participate in program evaluation</li>
-                <li>Assume responsibilities as assigned</li>
             </ul>
         </div>
 
-        <!-- Deliverables -->
-        <div>
-            <div class="text-[#5273FA] my-6 mt-10 text-2xl font-semibold font-playfair">Deliverables</div>
-            <ul class="list-outside list-disc ml-4">
-                <li>20-30 pages long academically written paper with policy and action recommendations</li>
-                <li>Executive Summary of the project that outlines the main findings of the study and solution
-                    recommendations
-                </li>
-                <li>PowerPoint slides for final research/project presentation</li>
-                <li>Raw data and all project related materials. E.g. survey forms, articles, journals, etc.</li>
-            </ul>
+        <div class="mt-8 block">The CRPI Policy Action Research will be carried out through NUUM and carries up to 9
+            credit hours.
         </div>
 
-        <div class="mt-8">
-            Interested candidates should submit a resume and a one-page cover letter to Human Resources
-            at HR@thebaci.org. Applications will be reviewed immediately.
+        <div class="mt-8  font-bold"> To join the program &#8594; <a href="#"
+                class="bg-[#EDEDED] rounded-lg px-8 py-2 hover:bg-blue-400 transition duration-200 ease-in-out block lg:inline mt-4 lg:mt-0">see
+                the
+                announcement the CRPI
+                2023</a>
         </div>
+
+
     </div>
-
-    <?php include './templates/share-news-programs.php' ?>
 
     <?php include './components/relatedNewsAndPrograms.php'; ?>
 
